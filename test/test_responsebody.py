11b1584269c476c1ea6ed5f4cc87782a07715fce
shin04 <daikon0413@gmail.com>
2020-10-02 22:14:56 +0900

    #12 test template

---
+++ test/test_responsebody.py
@@ -0,0 +1,36 @@
+import unittest
+import sys
+import os
+sys.path.append(os.path.abspath(".."))
+
+import app  # noqa
+
+
+class TestStatusCode(unittest.TestCase):
+    def setUp(self):
+        print('Start Test About Response Body')
+        app.app.config['TESTING'] = True
+        self.client = app.app.test_client()
+
+    def tearDown(self):
+        print('End Test')
+
+    def test_hello(self):
+        res = self.client.get('/')
+        json_data = res.get_json()
+        message = json_data['message']
+        self.assertEqual(message, 'hello world')
+
+    def test_analyze(self):
+        print('[POST] /analyze')
+        res = self.client.post('/analyze', json={
+            'people_num': 2
+        })
+        json_data = res.get_json()
+        print(json_data)
+        message = json_data['error']['message']
+        self.assertEqual(message, 'parameter [file] is not found')
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test/test_statuscode.py
@@ -0,0 +1,32 @@
+import unittest
+import sys
+import os
+sys.path.append(os.path.abspath(".."))
+
+import app  # noqa
+
+
+class TestStatusCode(unittest.TestCase):
+    def setUp(self):
+        print('Start Test About Status Code')
+        app.app.config['TESTING'] = True
+        self.client = app.app.test_client()
+
+    def tearDown(self):
+        print('End Test')
+
+    def test_hello(self):
+        print('[GET] /')
+        res = self.client.get('/')
+        self.assertEqual(res.status, '200 OK')
+
+    def test_analyze(self):
+        print('[POST] /analyze')
+        res = self.client.post('/analyze', json={
+            'people_num': 2
+        })
+        self.assertEqual(res.status, '200 OK')
+
+
+if __name__ == '__main__':
+    unittest.main()
