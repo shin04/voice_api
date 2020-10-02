179ad4e532f7042b69176dde0ecefcc16de53b70
shin04 <daikon0413@gmail.com>
2020-10-02 20:15:44 +0900

    #10 error handling

--- app.py
+++ app.py
@@ -16,6 +16,17 @@ os.makedirs(app.config['FILE_PATH'], exist_ok=True)
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '../voice-recognition-288501-df448cb420f2.json'
 
 
+@app.errorhandler(400)
+def error_handler(error):
+    res = jsonify({
+        'error': {
+            'message': error.description['message']
+        },
+        'code': error.code
+    })
+    return res, error.code
+
+
 @app.route('/', methods=['GET'])
 def hello():
     return jsonify({'message': 'hello world'})
@@ -23,19 +34,18 @@ def hello():
 
 @app.route('/analyze', methods=['POST'])
 def analyze():
+    if 'people_num' not in request.form:
+        abort(400, {'message': 'parameter [people_num] is not found'})
+    elif request.form['people_num'] == '':
+        abort(400, {'message': 'people_num is not found'})
     people_num = int(request.form['people_num'])
 
     if 'files' not in request.files:
-        print('file not found')
-        res = {'success': True, 'message': 'file not found'}
-        return jsonify(res)
-
+        abort(400, {'message': 'parameter [file] is not found'})
     file = request.files['files']
 
     if file.filename == '':
-        print('file not found')
-        res = {'success': True, 'message': 'file not found'}
-        return jsonify(res)
+        abort(400, {'message': 'file is not found'})
 
     if file and allwed_file(file.filename, app.config):
         filename = file.filename
@@ -44,6 +54,10 @@ def analyze():
         res['success'] = True
         res['message'] = 'success'
         return jsonify(res)
+    elif allwed_file(file.filename, app.config):
+        abort(400, {'message': 'extension is invalid'})
+    else:
+        abort(400, {'message': 'unknown error'})
 
 
 if __name__ == '__main__':
