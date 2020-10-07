bcd0435968e5e053155efced8299c6ed7f3f1760
shin04 <daikon0413@gmail.com>
2020-10-07 12:05:51 +0900

    edit start.heroku.sh

--- start.heroku.sh
+++ start.heroku.sh
@@ -2,7 +2,5 @@
 echo Setting...
 mkdir credentials
 touch credentials/voice-recognition.json
-echo ${GOOGLE_APPLICATION_CREDENTIALS}
 echo ${GOOGLE_APPLICATION_CREDENTIALS} > credentials/voice-recognition.json
-cat credentials/voice-recognition.json
 python app.py
